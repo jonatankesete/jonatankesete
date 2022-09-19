d0ffc3a750da4789782775007b06badd5235283c
jonatankesete <96500672+jonatankesete@users.noreply.github.com>
2022-09-19 16:55:38 +0200

    Update räkna_ut_veckodag.py

--- räkna_ut_veckodag.py
+++ räkna_ut_veckodag.py
@@ -1,12 +1,12 @@
 
-#Jonatan Kesete, 990724-9099
+#Jonatan Kesete
 
-day = ['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']
+days = ['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday']
 
-months = ['jan','feb','mars','april','maj','juni','juli','aug','sep','okt','nov','dec']
+months = ['Januari','Februari','Mars','April','Maj','Juni','Juli','Augusti','September','Oktober','November','December']
 
 maxdagar = [31,28,31,30,31,30,31,30,31,30,31]
-
+            
 year = int(input('Year: '))
 while year < 1583 or year > 9999:
     print ('Out of allowed range 1583 to 9999')
@@ -19,50 +19,33 @@ while month  <  1 or month  > 12:
 
 
 day = int(input('Day: '))
-if year %400 == 0 or ( year %4 == 0 or year %100 !=0):
-    while month == 2 and (day  <  1 or day  > 29):
-         print ('Out of allowed range 1 to 29')
+if month in (1,3,5,7,8,10,12):
+    while day <  1 or day > 31:
+        print ('Out of allowed range 1 to 31')
+        day = int(input('Day: '))
 
-while month in (1,3,5,7,8,10,12) and (day <  1 or day> 31):
-    print ('Out of allowed 1 to 32')
-    day = int(input('Day: '))
-
-while month in (4,6,9,11) and (day <  1 or day  > 30):
-    print ('Out of allowed 1 to 30')
-    day = int(input('Day: '))
+elif month in (4,6,9,11):
+    while day  <  1 or day > 30:
+        print ('Out of allowed range 1 to 30')
+        day = int(input('Day: '))
 
 else:
-    while month == 2 and (day <  1 or day  > 28):
-        print ('Out of allowed 1 to 28')
-        day = int(input('Day: '))
+    if year % 400 == 0 or ( year % 4 == 0 and year % 100 != 0):
+        while day  <  1 or day  > 29:
+            print ('Out of allowed range 1 to 29') 
+            day = int(input('Day: '))
+    else:
+        while day <  1 or day  > 28:
+            print ('Out of allowed range 1 to 28')
+            day = int(input('Day: '))
 
-    while month in (1,3,5,7,8,10,12) and (day <  1 or day  > 31):
-        print ('Out of allowed 1 to 31')
-        day = int(input('Day: '))
-
+   
 if month == 1 or month == 2:
     month += 12
     year -= 1
     
-weekday = ( day + 13*(month+1)//5 + year + year//4
- - year//100 + year//400 ) % 7
-if weekday == 0:
-    print ('It is a Saturday')
+weekday = ( day + 13*(month+1)//5 + year + year//4 - year//100 + year//400 ) % 7
 
-elif weekday == 1:
-    print ('It is a Sunday')
 
-elif weekday == 2:
-    print ('It is a Monday')
+print ('It is a',days[weekday])
 
-elif weekday == 3:
-    print ('It is a Tuesday')
-
-elif weekday == 4:
-    print ('It is a Wednesday')
-    
-elif weekday == 5:
-    print ('It is a Thursday')
-
-elif weekday == 6:
-    print ('It is a Friday')
